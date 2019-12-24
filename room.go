8dc1184f64841b46e78bef60a231a6af42b1b9bd
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-12-24 12:52:50 -0700

    added more documentation to the room struct

--- room.go
+++ room.go
@@ -1,9 +1,16 @@
 package main
 
+// in the room struct we hold the messages that we want to send to the clients in a channel to forward the msg
+// we have the current state of our current clients of the room in a clients map
+// we manage joining and leaving that room through the leave and join channels for their respective purposes
+// one of the features that come with using different channels for joining and leaving the client map is that each of them is a different pipeline which means that we won't be accessing the same data resulting in safe opreations without having to worry about corrupt source of truth
 type room struct {
 	// forward is a channel that holds incoming messages that will go to other clients
 	forwardMsg chan []byte
-	join       chan *client
-	leave      chan *client
-	clients    map[*client]bool
+	// channel for clients joining the room
+	join chan *client
+	// channel for clients leaving the room
+	leave chan *client
+	// map of the current clients of this room
+	clients map[*client]bool
 }
