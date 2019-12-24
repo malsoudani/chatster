b278e62b099bc7bdb7aacf275731e2659d4aef7f
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-12-24 11:30:54 -0700

    creating a server and a template

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// start the web server
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal("server error: ", err)
+	}
+}
