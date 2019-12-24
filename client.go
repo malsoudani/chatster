4f27736ff3ecbd6f4aebb6645a6708b4ed3d495a
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-12-24 12:33:21 -0700

    impletemented the read and the write messages from the client prespective

--- client.go
+++ client.go
@@ -6,7 +6,28 @@ type client struct {
 	// socket is the websocket for the client
 	socket *websocket.Conn
 	// send is the channel on which the messages are sent
-	send chan []btye
+	msgToSend chan []btye
 	// room is the room this client is chatting in
 	room *room
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.msgToSend {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
 }
--- room.go
+++ room.go
@@ -2,5 +2,5 @@ package main
 
 type room struct {
 	// forward is a channel that holds incoming messages that will go to other clients
-	forward chan []byte
+	msgToForward chan []byte
 }
